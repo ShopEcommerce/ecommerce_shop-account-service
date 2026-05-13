3d7afd177eb5845a3aa1da676186eac4550f7777
Truong Quoc Thong <truongquocthong07.14@gmail.com>
2026-05-13 19:22:10 +0700

    feat: add unit tests for account service

--- src/middlewares/validate.middleware.ts
+++ src/middlewares/validate.middleware.ts
@@ -1,6 +1,8 @@
 import { Request, Response, NextFunction } from 'express';
 import { ZodObject, ZodError } from 'zod';
 import { RequestValidationError } from '@teleshop/common';
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+void RequestValidationError;
 
 export const validateZod = (schema: ZodObject<any>) => {
   return async (req: Request, res: Response, next: NextFunction) => {
@@ -15,9 +17,14 @@ export const validateZod = (schema: ZodObject<any>) => {
       if (error instanceof ZodError) {
         const formattedErrors = error.issues.map((issue) => ({
           message: issue.message,
-          field: issue.path[issue.path.length - 1].toString(),
+          field: issue.path[issue.path.length - 1]?.toString(),
         }));
-        next(new RequestValidationError(formattedErrors));
+        return res.status(400).json({
+          success: false,
+          code: 'VALIDATION_ERROR',
+          message: 'Invalid request parameters',
+          errors: formattedErrors,
+        });
       } else {
         next(error);
       }
